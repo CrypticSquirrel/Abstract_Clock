d891cde1eead9b4bc7102de12d95d3c941f5236d
MINKIM-92 <dhflqksoto@gmail.com>
2020-05-13 00:28:58 -0400

    Creating circle by using ellipse

--- main.js
+++ main.js
@@ -12,9 +12,20 @@ function draw() {
     let hr = hour();
     let mn = minute();
     let sd = second();
+/**
+ * Setting up the clock.
+ */
+    // fill(255);
+    // noStroke();
+    // text(hr + ':' + mn + ':' + sd, 10 , 200);
 
-    fill(255);
-    noStroke();
-    text(hr + ':' + mn + ':' + sd, 10 , 200);
+    /**
+     * Creating the circle.
+     */
+    strokeWeight(4); // witdh of the stroke used for line.
+    stroke(255);
+    noFill();
+    ellipse(200,200,300,300);
+
 }
     
