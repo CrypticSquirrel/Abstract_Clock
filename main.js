740e03ba6cb5c7aecd11a73de2ddef7a7920b64b
MINKIM-92 <dhflqksoto@gmail.com>
2020-05-13 23:38:01 -0400

    modifying the colors. make a moving circle.

--- main.js
+++ main.js
@@ -39,10 +39,18 @@ function draw() {
 
     // MouseX. The system variable mouseX always contains the current horizontal position of the mouse, 
     //relative to (0, 0) of the canvas.
-    //noFill();
-    let end = map(mouseX, 0, width, 0, 360);
+    noFill();
+    let end1 = map(sd, 0, 60, 0, 360);
    // arc(200, 200, 300, 300, 0, end, PIE); // a semicircle . 
-    arc(200, 200, 300, 300, 0, end, OPEN);
+    //arc(200, 200, 300, 300, 0, end, OPEN);
+    arc(200, 200, 300, 300, 0, end1);
 
+    stroke(150, 100, 255);
+    let end2 = map(mn, 0, 60, 0, 360);
+    arc(200, 200, 280, 280, 0, end2);
+    
+    stroke(150, 255, 100);
+    let end3 = map(hr, 0, 24, 0, 360);
+    arc(200, 200, 280, 280, 0, end3);
 }
     
