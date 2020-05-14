fd8947d5097503299c88600a1de487bd7ddabbe6
MINKIM-92 <dhflqksoto@gmail.com>
2020-05-13 23:32:24 -0400

    Using map, mouseX and the arc style.

--- main.js
+++ main.js
@@ -23,20 +23,26 @@ function draw() {
     /**
      * Creating the circle.
      */
-    strokeWeight(4); // witdh of the stroke used for line.
-    stroke(255);
-    noFill();
-    ellipse(200,200,300,300);
+    // strokeWeight(8); // witdh of the stroke used for line.
+    // stroke(255);
+    // noFill();
+    // ellipse(200,200,300,300);
 
     
-
+    strokeWeight(4);
     stroke(255, 100, 150);
     /**
-     * x, y, distance(r x), distance(r,x) but the last one is angle.
-     * we can make a half of circle by modifying the angle.
+     * x, y, distance(r x), distance(r,x) bu t the last one is angle.
+     * we can make a semicircle by modifying the angle.
      */
-    arc(200, 200, 300, 300, 0, 360); 
+    // arc(200, 200, 300, 300, 0, 360); // Circle.
 
+    // MouseX. The system variable mouseX always contains the current horizontal position of the mouse, 
+    //relative to (0, 0) of the canvas.
+    //noFill();
+    let end = map(mouseX, 0, width, 0, 360);
+   // arc(200, 200, 300, 300, 0, end, PIE); // a semicircle . 
+    arc(200, 200, 300, 300, 0, end, OPEN);
 
 }
     
