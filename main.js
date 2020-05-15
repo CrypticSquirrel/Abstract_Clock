43a5d6acebba9e10e55e4622ad3e2a4cdc2b0238
MINKIM-92 <dhflqksoto@gmail.com>
2020-05-14 21:23:45 -0400

    create the line

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ function setup() {
 
 function draw() {
     background(0);
-
+    translate(200, 200);
+    rotate(-90)
     let hr = hour();
     let mn = minute();
     let sd = second();
@@ -43,14 +44,20 @@ function draw() {
     let end1 = map(sd, 0, 60, 0, 360);
    // arc(200, 200, 300, 300, 0, end, PIE); // a semicircle . 
     //arc(200, 200, 300, 300, 0, end, OPEN);
-    arc(200, 200, 300, 300, 0, end1);
+    arc(0, 0, 300, 300, 0, end1);
+    
+    /**
+     * creating the line!
+     */
+    stroke(255)
+    line(0,0,100,0)
 
     stroke(150, 100, 255);
     let end2 = map(mn, 0, 60, 0, 360);
-    arc(200, 200, 280, 280, 0, end2);
+    arc(0, 0, 280, 280, 0, end2);
     
     stroke(150, 255, 100);
-    let end3 = map(hr, 0, 24, 0, 360);
-    arc(200, 200, 280, 280, 0, end3);
+    let end3 = map(hr%12, 0, 12, 0, 360);
+    arc(0, 0, 260, 260, 0, end3);
 }
     
